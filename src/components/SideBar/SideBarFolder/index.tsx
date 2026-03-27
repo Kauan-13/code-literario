d19d0c4cdbd59d9634e20049ae73d704d0fb656f
Kauan Oliveira Freitas <kauanffreitas57@gmail.com>
2026-03-27 03:30:30 -0300

    removendo tag li desnecessária

--- src/components/SideBar/SideBarFolder/index.tsx
+++ src/components/SideBar/SideBarFolder/index.tsx
@@ -18,53 +18,50 @@ const SideBarFolder = ({genre, posts}: Props) => {
 
     return (
         <ul>
-            <li className={style.liFolder}>
-                <div className={`${style.items} ${style.folderItem}`} 
-                    onClick={() => {
-                        setContentVisibility(true)
-                        navigate("/folder/" + genre)
-                    }}>
-                    <AnimatePresence mode="wait" initial={false}>
-                        <motion.div
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                setContentVisibility(v => !v)
-                            }}
-                            key={contentVisibility ? "open" : "close"}
-                            initial={{rotate: 90 }}
-                            animate={{rotate: 0 }}
-                            exit={{rotate: 0 }}
-                            transition={{ duration: 0.2 }}
-                        >
-                            {contentVisibility ? <IoIosArrowDown/> : <IoIosArrowForward/>}
-                        </motion.div>
-                    </AnimatePresence>
-                    <AnimatePresence mode="wait" initial={false}>
-                        <motion.div
-                            key={contentVisibility ? "open" : "close"}
-                            initial={{ y: 2, opacity: 1, rotate: 0 }}
-                            animate={{ y: 0, opacity: 1, rotate: 0 }}
-                            exit={{ y: 2, opacity: 1, rotate: 0 }}
-                            transition={{ duration: 0.1 }}
-                        >
-                            {contentVisibility ? <FaFolderOpen/> : <FaFolder/>}
-                        </motion.div>
-                    </AnimatePresence>
-                    <p>{genre}</p>
-                </div>
-                {
-                    contentVisibility ?
-                        <ul className={style.ulFiles}>
-                            {
-                                posts.map((post, key) => (
-                                    <FileItem key={key} title={post.title} id={post.id} className={style.fileItem}/>
-                                ))
-                            }
-                        </ul>
-                    : null
-                }
-                
-            </li>
+            <div className={`${style.items} ${style.folderItem}`} 
+                onClick={() => {
+                    setContentVisibility(true)
+                    navigate("/folder/" + genre)
+                }}>
+                <AnimatePresence mode="wait" initial={false}>
+                    <motion.div
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            setContentVisibility(v => !v)
+                        }}
+                        key={contentVisibility ? "open" : "close"}
+                        initial={{rotate: 90 }}
+                        animate={{rotate: 0 }}
+                        exit={{rotate: 0 }}
+                        transition={{ duration: 0.2 }}
+                    >
+                        {contentVisibility ? <IoIosArrowDown/> : <IoIosArrowForward/>}
+                    </motion.div>
+                </AnimatePresence>
+                <AnimatePresence mode="wait" initial={false}>
+                    <motion.div
+                        key={contentVisibility ? "open" : "close"}
+                        initial={{ y: 2, opacity: 1, rotate: 0 }}
+                        animate={{ y: 0, opacity: 1, rotate: 0 }}
+                        exit={{ y: 2, opacity: 1, rotate: 0 }}
+                        transition={{ duration: 0.1 }}
+                    >
+                        {contentVisibility ? <FaFolderOpen/> : <FaFolder/>}
+                    </motion.div>
+                </AnimatePresence>
+                <p>{genre}</p>
+            </div>
+            {
+                contentVisibility ?
+                    <ul className={style.ulFiles}>
+                        {
+                            posts.map((post, key) => (
+                                <FileItem key={key} title={post.title} id={post.id} className={style.fileItem}/>
+                            ))
+                        }
+                    </ul>
+                : null
+            }
         </ul>  
     )
 }
